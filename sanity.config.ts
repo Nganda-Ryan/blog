40fbdc1669b1f6582b7def7589ed7a3d85c44d94
Rai <rai.ebogo@laakam.it>
2025-01-16 00:35:32 +0100

    sharp

--- sanity.config.ts
+++ sanity.config.ts
@@ -9,7 +9,6 @@ import {defineConfig} from 'sanity'
 import { structureTool } from 'sanity/structure'
 import {codeInput} from '@sanity/code-input'
 
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
 import {schema} from './sanity/schemaTypes'
 import {structure} from './sanity/structure'
