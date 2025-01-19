12d0b5798edf204a432e1f675ee8f1b41cf797c8
Rai <rai.ebogo@laakam.it>
2025-01-19 01:59:47 +0100

    image resizing

--- components/sanity/SanityImage.tsx
+++ components/sanity/SanityImage.tsx
@@ -6,13 +6,15 @@ import { urlFor } from './../../sanity/sanity-utils';
 const SanityImage = ({ value }: { value: any }) => {
     const imageAlt = (urlFor(value).options.source as { alt: string }).alt;
     return (
-        <Image 
-        src={urlFor(value).url()} 
-        alt={imageAlt} 
-        width={500}
-        height={500}
-        className='w-full'
-        />
+        <div className="w-full flex justify-center items-center">
+            <Image 
+                src={urlFor(value).url()} 
+                alt={imageAlt} 
+                width={500}
+                height={500}
+                className='w-full max-w-xl mx-auto'
+            />
+        </div>
     );
 }
 
