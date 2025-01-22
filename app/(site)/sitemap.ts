01a1a3d877f91ae6df6bc0d6b71d7bdd7f4548e2
Rai <rai.ebogo@laakam.it>
2025-01-22 18:30:58 +0100

    sitemap improved

--- app/(site)/sitemap.ts
+++ app/(site)/sitemap.ts
@@ -5,7 +5,7 @@ import { getAllPosts } from './../../sanity/sanity-utils'
 export const dynamic = 'force-static'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const siteUrl = siteMetadata.siteUrl
+  const siteUrl = siteMetadata.siteUrl;
   const sanityPosts = await getAllPosts();
   const blogRoutes = sanityPosts.filter((post) => !post.draft).map((post) => {
     const slug = post.slug?.current ? post.slug.current : "default-slug";
@@ -15,7 +15,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     }
   })
 
-  const routes = ['', 'blog', 'tags'].map((route) => ({
+  const routes = ['', 'blog', 'tags', 'about'].map((route) => ({
     url: `${siteUrl}/${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
